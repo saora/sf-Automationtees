1262febe63b2afe293336447c2cb5a72383a7889
saoramx <saora.mx@gmail.com>
2019-11-25 02:11:38 -0600

    so. actualizacion de clases

--- src/test/java/steps/login/LoginSteps.java
+++ src/test/java/steps/login/LoginSteps.java
@@ -1,6 +1,8 @@
 package steps.login;
 
+import cucumber.api.java.Before;
 import net.thucydides.core.annotations.Step;
+import org.joda.time.Seconds;
 import pages.login.LeccLoginPage;
 import pages.login.SfLoginPage;
 import pages.salesforce.SfHomePage;
@@ -12,27 +14,37 @@ public class LoginSteps {
     private SfLoginData sfLoginData;
     private LeccLoginPage leccLoginPage;
     private LeccLoginData leccLoginData;
+    private SfHomePage sfHomePage;
 
     @Step
-    public void openLoginPage() {
+    public void openLoginPage(String urlapplication) {
         sfLoginPage.getDriver().manage().window().maximize();
-        sfLoginPage.open();
+        sfLoginPage.getDriver().get(urlapplication);
+
+       // sfLoginPage.open();
     }
 
     @Step
-    public void openLeccLoginPage(){
-        leccLoginPage.open();
-        leccLoginPage.getDriver().manage().window().maximize();
-    }
-
-    @Step
-    public void sendLoginData()throws Exception {
+    public void sendLoginData() {
         //sfLoginData.loginSalesforce();
         sfLoginData.sfEnterLoginData();
     }
 
+    public void openLeccLoginPage(){
+        leccLoginPage.openLecc();
+    }
+
     @Step
-    public void sendLeccLoginData()throws Exception{
+    public void verificationCodeForLogin(){
+        sfLoginData.verificationCodebyEmail();
+    }
+    @Step
+    public void switchToClassicMode(){
+        sfHomePage.switchToClassic();
+    }
+
+    @Step
+    public void sendLeccLoginData(){
         //leccLoginData.loginLecc();
           sfLoginData.sfEnterLoginData();
 
