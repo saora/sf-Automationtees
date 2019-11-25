829e58d4bdff41741b0a4b3f40e71e682bd261c3
Salvador Ortuno <saora.mx@gmail.com>
2019-11-25 13:13:40 -0600

    actualizacion de clase login data

--- src/test/java/stepsdefinition/lecc/LeccLoginStepsDefinition.java
+++ src/test/java/stepsdefinition/lecc/LeccLoginStepsDefinition.java
@@ -23,7 +23,7 @@ public class LeccLoginStepsDefinition {
     }
     @When("^I submit the username and password for lecc$")
     public void fillUserData(){
-        loginSteps.sendLoginData();
+        loginSteps.sendLoginData("lecc.login");
         //loginSteps.sendLeccLoginData();
     }
     @Then("^I should Logged In Lecc home page$")
