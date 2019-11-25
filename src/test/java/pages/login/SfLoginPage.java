e983e979fa6cc71603678c2e99fc42958f309b76
saoramx <saora.mx@gmail.com>
2019-11-25 02:09:49 -0600

    so. actualizacion de clases

--- src/test/java/pages/login/SfLoginPage.java
+++ src/test/java/pages/login/SfLoginPage.java
@@ -1,18 +1,22 @@
 package pages.login;
 
-import jdk.jfr.Timespan;
 import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.PageObject;
 import net.serenitybdd.core.pages.WebElementFacade;
-import net.thucydides.core.annotations.DefaultUrl;
+import org.jruby.RubyProcess;
 import org.junit.Assert;
+import org.openqa.selenium.By;
+
+import java.time.Duration;
+
+import static java.time.temporal.ChronoUnit.SECONDS;
 
 
 //@DefaultUrl("https://login.stmpa.stm.salesforce.com/")
-@DefaultUrl("https://login.salesforce.com/?locale=eu")
+//@DefaultUrl("https://login.salesforce.com/?locale=eu")
 public class SfLoginPage extends PageObject {
     @FindBy(id = "username")
-    private WebElementFacade user;
+    private static WebElementFacade user;
 
     @FindBy(id = "password")
     private WebElementFacade pass;
@@ -34,13 +38,23 @@ public class SfLoginPage extends PageObject {
     private WebElementFacade salesforceClassicHomeLogo;
 
 
+    @FindBy(xpath = "/html/body/div[3]/div/div[4]")
+    private WebElementFacade loginRightPanel;
+
+    @FindBy(className = "btn-container ")
+    private WebElementFacade btnfreetrial;
+
     public void setLoginData(String user, String pass){
+
+        $("#username").waitUntilVisible();
+
+
         this.user.sendKeys(user);
         this.pass.sendKeys(pass);
     }
 
     public void setOrgUser(String orgUser){
-        this.user.sendKeys(orgUser);
+        user.sendKeys(orgUser);
     }
 
     public void setOrgPass(String orgPass){
@@ -48,21 +62,27 @@ public class SfLoginPage extends PageObject {
     }
 
     public void submitBtn(){
-        this.submiSftBtn.click();
+            submiSftBtn.click();
+        //withTimeoutOf(Duration.ofSeconds(30)).find(By.id("Login")).click();
     }
 
     public void setVerificationCode(String verifyCode){
-        fillVerificationCode.sendKeys(verifyCode);
-        click_Verify.click();
+        fillVerificationCode.withTimeoutOf(5,SECONDS).waitUntilPresent().sendKeys(verifyCode);
     }
+
+    public void submmitVerificationCode(){
+        click_Verify.withTimeoutOf(5,SECONDS).waitUntilClickable().click();
+}
 
 
     public void salesforceLoginVerification(){
-        if(salesforceLightningHomeLogo.isCurrentlyVisible()){
-            Assert.assertTrue(salesforceLightningHomeLogo.isCurrentlyVisible());
-        }else{
-            Assert.assertTrue(salesforceClassicHomeLogo.isCurrentlyVisible());
-        }
+        Assert.assertTrue(salesforceClassicHomeLogo.isDisplayed());
+
+//        if(salesforceLightningHomeLogo.isCurrentlyVisible()){
+//            Assert.assertTrue(salesforceLightningHomeLogo.isCurrentlyVisible());
+//        }else{
+//            Assert.assertTrue(salesforceClassicHomeLogo.isCurrentlyVisible());
+//        }
 
     }
 
