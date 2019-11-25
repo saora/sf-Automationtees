5f158c34444db6fd53a8f378e879a1af11a297f8
saoramx <saora.mx@gmail.com>
2019-11-25 02:14:24 -0600

    so. actualizacion de clases

--- src/test/java/testdata/LayoutData.java
+++ src/test/java/testdata/LayoutData.java
@@ -2,16 +2,33 @@ package testdata;
 
 import com.FileDataReader;
 import net.serenitybdd.core.pages.PageObject;
+import pages.salesforce.LayoutPage;
 import pages.salesforce.SfHomePage;
 
 import java.io.IOException;
 
 public class LayoutData extends PageObject {
+    private LayoutPage layoutPage;
     private SfHomePage sfHomePage;
 
-    public void getLayoutName() throws IOException {
-        FileDataReader prop = new FileDataReader();
-        sfHomePage.addCustomLayout(prop.propertiesFile().getProperty("layout.name"));
+   private FileDataReader prop = new FileDataReader();
+
+    public void getLayoutName() {
+        try {
+            layoutPage.addCustomLayout(prop.propertiesFile().getProperty("layout.name"));
+        }catch (IOException e){
+            System.out.println("Fail getting layout name!!");
+        }
     }
+
+    public void addJsButtonToCustomLayout(){
+        try{
+            layoutPage.dragAndDropJsButtonToLayout(prop.propertiesFile().getProperty("jsbutton.name"));
+        }catch (IOException e){
+            System.out.println("Fail getting Js Button name!!");
+        }
+
+    }
+
 
 }
