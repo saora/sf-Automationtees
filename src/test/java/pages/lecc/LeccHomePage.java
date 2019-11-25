0e2f5744678d803cfd5cd5715e762c816b872ae4
Salvador Ortuno <saora.mx@gmail.com>
2019-11-25 17:38:49 -0600

    actualizacion de clase login data

--- src/test/java/pages/lecc/LeccHomePage.java
+++ src/test/java/pages/lecc/LeccHomePage.java
@@ -3,20 +3,24 @@ package pages.lecc;
 import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.PageObject;
 import net.serenitybdd.core.pages.WebElementFacade;
+import org.jruby.RubyProcess;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
 
 public class LeccHomePage extends PageObject {
-    @FindBy(xpath = "//*[@id=\"form-j_idt64\"]/span")
+    @FindBy(id = "form-j_idt62")
     private WebElementFacade jsButtonScan;
 
-    @FindBy(id = "form-j_idt88")
+    @FindBy(id = "form-j_idt91")
     private WebElementFacade jsButtonConfirm;
 
     @FindBy(id = "form-j_idt72")
     private WebElementFacade inProgress;
+
+    @FindBy(id = "form-j_idt81")
+    private WebElementFacade scanResult;
 
     public void jsBtnScan(){
         //waitForCondition().until(ExpectedConditions.elementToBeClickable(jsButtonScan));
@@ -30,5 +34,10 @@ public class LeccHomePage extends PageObject {
     public void scanVerification(){
         waitForCondition().until(ExpectedConditions.textToBePresentInElement(inProgress,"Scan in progress"));
         inProgress.getText();
+    }
+
+    public void scanResult(){
+        scanResult.getText();
+        System.out.println("RESULT: "+scanResult.getText());
     }
 }
