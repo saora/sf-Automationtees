48784aa1802ea85df1e2281e0ed7f2a6b7d262e3
saoramx <saora.mx@gmail.com>
2019-11-26 02:56:51 -0600

    so. scan js buttons

--- src/test/java/pages/lecc/LeccHomePage.java
+++ src/test/java/pages/lecc/LeccHomePage.java
@@ -4,8 +4,11 @@ import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.PageObject;
 import net.serenitybdd.core.pages.WebElementFacade;
 import org.jruby.RubyProcess;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.util.concurrent.TimeUnit;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
 
@@ -20,24 +23,45 @@ public class LeccHomePage extends PageObject {
     private WebElementFacade inProgress;
 
     @FindBy(id = "form-j_idt81")
-    private WebElementFacade scanResult;
+    private WebElementFacade scanResultYellowIcon;
+
+    @FindBy(id = "form-j_idt78")
+    private WebElementFacade getScanResultGreenIcon;
+
+    @FindBy(className = "slds-text-align_center slds-p-around_medium slds-text-heading_small")
+    private WebElementFacade textResultIcon;
+
+    @FindBy(className = "ui-outputpanel ui-widget")
+    private WebElementFacade outputPanelScanResult;
 
     public void jsBtnScan(){
-        //waitForCondition().until(ExpectedConditions.elementToBeClickable(jsButtonScan));
-        //waitFor("//*[@id=\"form-j_idt64\"]/span");
-        //System.out.println();
-
-        jsButtonScan.click(); }
+       // waitForCondition().until(ExpectedConditions.elementToBeClickable(jsButtonScan));
+        getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        jsButtonScan.waitUntilClickable().click();
+    }
     public void confirmScan(){
+        getDriver().switchTo().activeElement();
         jsButtonConfirm.click();
+        //jsButtonConfirm.click();
     }
     public void scanVerification(){
-        waitForCondition().until(ExpectedConditions.textToBePresentInElement(inProgress,"Scan in progress"));
+       // waitForCondition().until(ExpectedConditions.textToBePresentInElement(inProgress,"Scan in progress"));
         inProgress.getText();
     }
 
     public void scanResult(){
-        scanResult.getText();
-        System.out.println("RESULT: "+scanResult.getText());
+        getDriver().manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+
+        //outputPanelScanResult.getText();
+        //System.out.println("RESULT: "+outputPanelScanResult.getText());
+        Assert.assertTrue(scanResultYellowIcon.isCurrentlyVisible());
+        try {
+            System.out.println("SCAN RESULT: "+scanResultYellowIcon.getText());
+        }catch (Exception e){
+            System.out.println("Fail for yellow icon!!");
+        }
+
+
+        //Assert.assertEquals(outputPanelScanResult.containsText("unconverted buttons are assigned but may not be used"),outputPanelScanResult.getText());
     }
 }
