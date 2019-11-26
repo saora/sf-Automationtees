1cdd510781fc453f72da2c5693351b0a81857141
saoramx <saora.mx@gmail.com>
2019-11-26 02:57:31 -0600

    so. scan js buttons

--- src/test/java/testdata/SfLoginData.java
+++ src/test/java/testdata/SfLoginData.java
@@ -5,6 +5,7 @@ import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.model.ValueRange;
 import net.serenitybdd.core.pages.PageObject;
 import org.jruby.RubyProcess;
+import org.openqa.selenium.ElementNotVisibleException;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import pages.login.LeccLoginPage;
 import pages.login.SfLoginPage;
@@ -49,25 +50,17 @@ public class SfLoginData extends PageObject {
             getDriver().get(prop.propertiesFile().getProperty(envLoginUrl));
          try {
              leccLoginPage.openLecc();
-         }catch (Exception e){
-             System.out.println("Element for LECC login not found !!");
+         }catch (ElementNotVisibleException e){
+             System.out.println("Element for LECC login not found !!"+e);
          }
-
             sfLoginPage.setOrgUser(prop.propertiesFile().getProperty("salesforce.username"));
             sfLoginPage.setOrgPass(prop.propertiesFile().getProperty("password.salesforce"));
         }catch (IOException e){
             System.out.println("Fail getting org user data!!");
         }
         sfLoginPage.submitBtn();
-
-        try{
-            leccLoginPage.allowLeccAccess();
-        }catch(Exception e){
-            System.out.println("Element for allow acces is not present !!");
-
-        }
-
     }
+
 
     public void verificationCodebyEmail(){
         try {
