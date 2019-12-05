d152de87dafc884000c9f785347c410ae8d4756b
Salvador Ortuno <saora.mx@gmail.com>
2019-12-04 20:53:03 -0600

    update for validations

--- src/main/java/com/FileDataReader.java
+++ src/main/java/com/FileDataReader.java
@@ -5,13 +5,19 @@ import java.util.Properties;
 
 public class FileDataReader implements InterfaceApplication{
 
-    public Properties propertiesFile()throws IOException{
-        Properties propfile = new Properties();
-        FileReader readFile = new FileReader(propertiesFile);
-        propfile.load(readFile);
-        return propfile;
-    }
+    public Properties propertiesFile() {
+        try {
+            Properties propfile = new Properties();
+            FileReader readFile = new FileReader(propertiesFile);
+            propfile.load(readFile);
 
+            return propfile;
+        } catch (IOException e) {
+           System.out.println("File not found!! "+e);
+        }
+       return propertiesFile();
+
+    }
 
 }
 
