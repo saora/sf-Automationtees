25caf2477db14b00bb026b773f6a86572ce48dc6
saoramx <saora.mx@gmail.com>
2019-12-06 03:25:15 -0600

    so. actualizacion de clases

---
+++ src/test/java/stepsdefinition/salesforce/NetworkAccessStepsDefinition.java
@@ -0,0 +1,31 @@
+package stepsdefinition.salesforce;
+
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import net.thucydides.core.annotations.Steps;
+import steps.login.LoginSteps;
+import testdata.TrustedIpNetworkAccess;
+
+import java.io.IOException;
+
+public class NetworkAccessStepsDefinition {
+    @Steps
+    private LoginSteps loginSteps;
+    private TrustedIpNetworkAccess trustedIpNetworkAccess;
+
+    @Given("I navigate to Saleforce Login page")
+    public void openSfLoginPage(){
+        loginSteps.openLoginPage("salesforce.login");
+        loginSteps.sendLoginData("Green");
+        loginSteps.verificationCodeForLogin();
+    }
+
+    @When("I submit the username and password")
+    public void fillDataLogin()throws IOException {
+        trustedIpNetworkAccess.setIPConfiguration();
+    }
+
+
+}
