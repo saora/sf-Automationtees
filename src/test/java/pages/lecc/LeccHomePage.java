f4ffcf8f7172477d09d9728f0ee27038792bba55
saoramx <saora.mx@gmail.com>
2019-12-09 01:39:01 -0600

    so. actualizacion de clases

--- src/test/java/pages/lecc/LeccHomePage.java
+++ src/test/java/pages/lecc/LeccHomePage.java
@@ -8,59 +8,181 @@ import org.junit.Assert;
 import java.time.Duration;
 
 public class LeccHomePage extends PageObject {
+
+    @FindBy(id = "spinner")
+    private WebElementFacade spinner;
+
+   // Javascript Buttons
+    @FindBy(id = "form-j_idt67")
+    private WebElementFacade outputPannelFirstScan_JSB;
+
     @FindBy(id = "form-j_idt62")
-    private WebElementFacade jsButtonScan;
+    private WebElementFacade clickScan_JSB;
 
     @FindBy(id = "form-j_idt91")
-    private WebElementFacade jsButtonConfirm;
+    private WebElementFacade clickLetsDoit_JSB;
 
     @FindBy(id = "form-j_idt72")
-    private WebElementFacade inProgress;
-
-    @FindBy(id = "form-j_idt81")
-    private WebElementFacade scanResultYellowIcon;
+    private WebElementFacade inProgress_JSB;
 
     @FindBy(id = "form-j_idt78")
-    private WebElementFacade getScanResultGreenIcon;
+    private WebElementFacade outputPannel_JSB;
 
-    @FindBy(xpath = "//*[@id=\"form-j_idt78\"]/div[2]")
-    private WebElementFacade textResultIcon;
+    // Actions & Buttons
+    @FindBy(id = "form-j_idt119")
+    private WebElementFacade outputPannelFirstScan_AAB;
 
-    @FindBy(css = ".slds-text-align_center:nth-child(2)")
-    private WebElementFacade outputPanelScanResult;
+    @FindBy(id = "form-j_idt98")
+    private WebElementFacade clickScan_AAB;
 
-    @FindBy(className = "slds-grid slds-grid_align-center")
-    private WebElementFacade iconContainer;
+    @FindBy(id = "form-j_idt126")
+    private WebElementFacade clickLetsDoit_AAB;
 
-    public void jsBtnScan(){
-        try {
-            System.out.println("BEFORE SCAN: " + outputPanelScanResult.getText());
-        } catch (Exception e) {
-            System.out.println("First scan ...!");
+    @FindBy(id = "form-j_idt108")
+    private WebElementFacade outputPannel_AAB;
+
+    @FindBy(id = "form-j_idt113")
+    private WebElementFacade inProgress_AAB;
+
+    // Visual force pages
+    @FindBy(id = "form-j_idt147")
+    private WebElementFacade outputPannelFirstScan_VFP;
+
+    @FindBy(id = "form-j_idt138")
+    private WebElementFacade outputPannel_VFP;
+
+    @FindBy(id = "form-j_idt133")
+    private WebElementFacade clickScan_VFP;
+
+    @FindBy(id = "form-j_idt162")
+    private WebElementFacade clickLetsDoit_VFP;
+
+    @FindBy(id = "form-j_idt152")
+    private WebElementFacade inProgress_VFP;
+
+    //Hard-Coded URLs
+    @FindBy(id = "form-j_idt185")
+    private WebElementFacade outputPannelFirstScan_HCU;
+
+    @FindBy(id = "form-j_idt169")
+    private WebElementFacade clickScan_HCU;
+
+    @FindBy(id = "form-j_idt195")
+    private WebElementFacade clickLetsDoit_HCU;
+
+    @FindBy(id = "form-j_idt173")
+    private WebElementFacade inProgress_HCU;
+
+    @FindBy(id = "form-j_idt182")
+    private WebElementFacade outputPannel_HCU;
+
+    //App Exchange Packages
+    @FindBy(id = "form-noScanPerformedAppex")
+    private WebElementFacade outputPannelFirstScan_AEP;
+
+    @FindBy(id = "form-j_idt202")
+    private WebElementFacade clickScan_AEP;
+
+    @FindBy(id = "form-j_idt229")
+    private WebElementFacade clickLetsDoit_AEP;
+
+    @FindBy(id = "form-j_idt210")
+    private WebElementFacade inProgress_AEP;
+
+    @FindBy(id = "form-j_idt216")
+    private WebElementFacade outputPannel_AEP;
+
+
+    public void javascriptButtonScan(){
+        clickScan_JSB.withTimeoutOf(Duration.ofSeconds(5)).waitUntilClickable().click();
+        if(outputPannelFirstScan_JSB.isCurrentlyVisible()){
+            System.out.println("First Js Button scan.. "+outputPannelFirstScan_JSB.getText());
+        }else{
+            System.out.println("Before scan.. "+outputPannel_JSB.getText());
         }
-        jsButtonScan.withTimeoutOf(Duration.ofSeconds(10)).click();
-    }
-    public void confirmScan(){
         getDriver().switchTo().activeElement();
-        jsButtonConfirm.click();
+        clickLetsDoit_JSB.waitUntilClickable().click();
+        inProgress_JSB.getText();
+        System.out.println("Status.. "+inProgress_JSB.getText());
+        outputPannel_JSB.withTimeoutOf(Duration.ofSeconds(90)).waitUntilVisible();
+        System.out.println("After scan.. .. "+outputPannel_JSB.getText());
     }
 
-    public void scanVerification(String scanType){
-        inProgress.getText();
-        System.out.println("STATUS: "+outputPanelScanResult.getText());
-        switch (scanType){
-            case "Green":
-                getScanResultGreenIcon.withTimeoutOf(Duration.ofSeconds(120)).waitUntilVisible();
-                break;
-            case "Yellow":
-                scanResultYellowIcon.withTimeoutOf(Duration.ofSeconds(120)).waitUntilVisible();
-                break;
+    public void actionAndButtonScan(){
+        clickScan_AAB.withTimeoutOf(Duration.ofSeconds(5)).waitUntilClickable().click();
+        if(outputPannelFirstScan_AAB.isCurrentlyVisible()){
+            System.out.println("First Actions and Buttons Scan.. "+outputPannelFirstScan_AAB.getText());
+        }else{
+            System.out.println("Before scan.. "+outputPannel_AAB.getText());
         }
-
-        System.out.println("AFTER SCAN: "+outputPanelScanResult.getText());
+        clickLetsDoit_AAB.waitUntilClickable().click();
+        inProgress_AAB.getText();
+        System.out.println("Status.. "+outputPannel_AAB.getText());
+        outputPannel_AAB.withTimeoutOf(Duration.ofSeconds(90)).waitUntilVisible();
+        System.out.println("After scan.. .. "+outputPannel_AAB.getText());
     }
 
-    public void scanResult(String textValidation) {
-            Assert.assertTrue(outputPanelScanResult.getText(),outputPanelScanResult.containsText(textValidation));
+    public void scanVisualforcePages(){
+        clickScan_VFP.withTimeoutOf(Duration.ofSeconds(5)).waitUntilClickable().click();
+        if(outputPannelFirstScan_VFP.isCurrentlyVisible()){
+            System.out.println("First Visualforce Pages scan.. "+outputPannelFirstScan_VFP.getText());
+        }else{
+            System.out.println("Before scan.. "+outputPannel_VFP.getText());
+
+        }
+        clickLetsDoit_VFP.waitUntilClickable().click();
+        inProgress_VFP.getText();
+        System.out.println("Status.. "+inProgress_VFP.getText());
+        outputPannel_VFP.withTimeoutOf(Duration.ofSeconds(90)).waitUntilVisible();
+        System.out.println("After scan.. .. "+outputPannel_VFP.getText());
+    }
+
+    public void scanHardCodedUrls(){
+        clickScan_HCU.withTimeoutOf(Duration.ofSeconds(5)).waitUntilClickable().click();
+        if(outputPannelFirstScan_HCU.isCurrentlyVisible()){
+            System.out.println("First Visualforce Pages scan.. "+outputPannelFirstScan_HCU.getText());
+        }else{
+            System.out.println("Before scan.. "+outputPannel_HCU.getText());
+
+        }
+        clickLetsDoit_HCU.waitUntilClickable().click();
+        inProgress_HCU.getText();
+        System.out.println("Status.. "+inProgress_HCU.getText());
+        outputPannel_HCU.withTimeoutOf(Duration.ofSeconds(90)).waitUntilVisible();
+        System.out.println("After scan.. .. "+outputPannel_HCU.getText());
+    }
+
+    public void scanAppExchangePackages(){
+        clickScan_AEP.withTimeoutOf(Duration.ofSeconds(5)).waitUntilClickable().click();
+        if(outputPannelFirstScan_AEP.isCurrentlyVisible()){
+            System.out.println("First Visualforce Pages scan.. "+outputPannelFirstScan_AEP.getText());
+        }else{
+            System.out.println("Before scan.. "+outputPannel_AEP.getText());
+        }
+        clickLetsDoit_AEP.waitUntilClickable().click();
+        inProgress_AEP.getText();
+        System.out.println("Status.. "+inProgress_AEP.getText());
+        outputPannel_AEP.withTimeoutOf(Duration.ofSeconds(90)).waitUntilVisible();
+        System.out.println("After scan.. .. "+outputPannel_AEP.getText());
+    }
+
+    public void scanJsButtonResult(String txtScanJSB) {
+            Assert.assertTrue(outputPannel_JSB.getText(),outputPannel_JSB.containsText(txtScanJSB));
+    }
+
+    public void scanResultActionsAndButtons(String txtScanAAB){
+        Assert.assertTrue(outputPannel_AAB.getText(),outputPannel_AAB.containsText(txtScanAAB));
+    }
+
+    public void scanResultVisualforcePages(String txtScanVFP){
+        Assert.assertTrue(outputPannel_VFP.getText(),outputPannel_VFP.containsText(txtScanVFP));
+    }
+
+    public void scanResultHardCodedUrls(String txtScanHCU){
+        Assert.assertTrue(outputPannel_HCU.getText(),outputPannel_HCU.containsText(txtScanHCU));
+    }
+
+    public void scanResultAppExchangePackages(String txtScanAEP){
+        Assert.assertTrue(outputPannel_AEP.getText(),outputPannel_AEP.containsText(txtScanAEP));
     }
 }
--- src/test/java/pages/login/LoginPage.java
+++ src/test/java/pages/login/LoginPage.java
@@ -1,20 +1,23 @@
 package pages.login;
 
+import com.FileDataReader;
+import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.model.ValueRange;
 import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.PageObject;
 import net.serenitybdd.core.pages.WebElementFacade;
+import net.thucydides.core.webdriver.exceptions.ElementShouldBeEnabledException;
 import org.jruby.RubyProcess;
 import org.junit.Assert;
-import org.openqa.selenium.By;
+import utils.InboxReader;
 
+import java.io.IOException;
 import java.time.Duration;
+import java.util.List;
 
-import static java.time.temporal.ChronoUnit.SECONDS;
+import static com.GetDataFromSpreadSheet.getSheetsService;
 
-
-//@DefaultUrl("https://login.stmpa.stm.salesforce.com/")
-//@DefaultUrl("https://login.salesforce.com/?locale=eu")
-public class SfLoginPage extends PageObject {
+public class LoginPage extends PageObject {
     @FindBy(id = "username")
     private static WebElementFacade user;
 
@@ -44,18 +47,25 @@ public class SfLoginPage extends PageObject {
     @FindBy(className = "btn-container ")
     private WebElementFacade btnfreetrial;
 
+    //LECC
+    @FindBy(id = "loginButton")
+    private WebElementFacade loginBtn;
 
-    public void setOrgUser(String orgUser){
+    @FindBy(xpath = "//*[@id='oaapprove']")
+    private WebElementFacade allowAccessToLeccHomePage;
+
+    private FileDataReader prop = new FileDataReader();
+
+    private void setOrgUser(String orgUser){
         user.sendKeys(orgUser);
     }
 
-    public void setOrgPass(String orgPass){
+    private void setOrgPass(String orgPass){
         this.pass.sendKeys(orgPass);
     }
 
-    public void submitBtn(){
+    private void submitBtn(){
             submiSftBtn.click();
-        //withTimeoutOf(Duration.ofSeconds(30)).find(By.id("Login")).click();
     }
 
     public void emailVerificationCode(String verifyCode){
@@ -63,11 +73,81 @@ public class SfLoginPage extends PageObject {
             click_Verify.withTimeoutOf(Duration.ofSeconds(5)).waitUntilClickable().click();
     }
 
-
-
     public void salesforceLoginVerification(){
         Assert.assertTrue(salesforceClassicHomeLogo.isDisplayed());
     }
 
+
+
+    public void EnterLoginData(String scanIcon) {
+        if(loginBtn.isCurrentlyVisible()){
+            loginBtn.click();
+        }else{
+            System.out.println("Button before Login page was not found!! ");
+        }
+
+        switch (scanIcon){
+            case "Green":
+                user.sendKeys(prop.propertiesFile().getProperty("green-org.username"));
+                pass.sendKeys(prop.propertiesFile().getProperty("green-org.password"));
+                break;
+            case "Yellow":
+                setOrgUser(prop.propertiesFile().getProperty("yellow-org.username"));
+                setOrgPass(prop.propertiesFile().getProperty("yellow-org.password"));
+                break;
+        }
+        submiSftBtn.click();
+        if(fillVerificationCode.isCurrentlyVisible()){
+            verificationCodebyEmail();
+        }else{
+            System.out.println("Code verification not needed!! ");
+        }
+    }
+
+    public void getUrlLogin(String loginUrl){
+        getDriver().manage().window().maximize();
+        getDriver().get(prop.propertiesFile().getProperty(loginUrl));
+    }
+
+
+    public void verificationCodebyEmail(){
+            try {
+                String colect = InboxReader.getEmail();
+                    System.out.println("Email Verification code: "+colect);
+                    fillVerificationCode.sendKeys(colect);
+                    click_Verify.waitUntilClickable().click();
+                    //emailVerificationCode(colect);
+            }catch (Exception e){
+                System.out.println("Fail while getting the verification code... "+e.getMessage());
+            }
+
+    }
+
+
+    public void allowLeccAccess(){
+        if(allowAccessToLeccHomePage.isCurrentlyVisible()){
+            allowAccessToLeccHomePage.withTimeoutOf(Duration.ofSeconds(5)).waitUntilClickable().click();
+        }else{
+            System.out.println("Button for allow acces is not present !!");
+        }
+    }
+
+
+    public void loginWithDataFromSpreadSheet() throws IOException {
+        Sheets service = getSheetsService();
+        String spreadsheetId = "1lCOOmjCjy2IvDf7DhQJvMnTvhlpHPwAx1YmBRraM0PU";
+        String range = "Login Test Data!A2:B";
+        ValueRange response = service.spreadsheets().values().get(spreadsheetId, range).execute();
+        List<List<Object>> values = response.getValues();
+        if (values == null || values.size() == 0) {
+            System.out.println("No data found.");
+        } else {
+            for (List row : values) {
+                setOrgUser(String.valueOf(row.get(0)));
+                setOrgPass(String.valueOf(row.get(1)));
+                submitBtn();
+            }
+        }
+    }
 
 }
