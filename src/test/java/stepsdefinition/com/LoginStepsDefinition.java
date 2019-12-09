9ed08c73db07e0da5f0a87bdca6532823796c049
saoramx <saora.mx@gmail.com>
2019-12-09 01:40:54 -0600

    so. actualizacion de clases

--- src/test/java/stepsdefinition/com/LoginStepsDefinition.java
+++ src/test/java/stepsdefinition/com/LoginStepsDefinition.java
@@ -4,41 +4,42 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import net.thucydides.core.annotations.Steps;
+import pages.login.LoginPage;
 import pages.salesforce.SfHomePage;
-import steps.login.LoginSteps;
 
 public class LoginStepsDefinition {
 
     @Steps
-    private LoginSteps loginSteps;
+    private LoginPage loginPage;
+    private SfHomePage sfHomePage;
 
     @Given("^I navigate to Salesforce Login page$")
     public void getSaleforceLoginPage(){
-        loginSteps.openLoginPage("salesforce.login");
+        loginPage.getUrlLogin("salesforce.login");
     }
 
     @Given("^I navigate to Lecc Login page$")
     public void getLeccLoginPage(){
-        loginSteps.openLoginPage("lecc.login");
+        loginPage.getUrlLogin("lecc.login");
     }
 
     @When("^I log in org with green icon$")
     public void orgWithGreenIcon() {
-        loginSteps.sendLoginData("Green");
-        loginSteps.verificationCodeForLogin();
-        loginSteps.allowAccessToLeccHomePages();
+        loginPage.EnterLoginData("Green");
+        //loginPage.verificationCodebyEmail();
+        loginPage.allowLeccAccess();
     }
 
     @When("^I log in org with yellow icon$")
     public void orgWithYellowIcon() {
-        loginSteps.sendLoginData("Yellow");
-        loginSteps.verificationCodeForLogin();
-        loginSteps.allowAccessToLeccHomePages();
+        loginPage.EnterLoginData("Yellow");
+       // loginPage.verificationCodebyEmail();
+        loginPage.allowLeccAccess();
     }
 
     @Then("^I should Logged In the salesforce page$")
     public void sfLoginValidation(){
-        loginSteps.switchToClassicMode();
+        sfHomePage.switchToClassic();
         //loginSteps.loginExpectedResult();
     }
 
