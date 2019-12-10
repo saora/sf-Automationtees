69abe06041e37d36be77c554d4d9a27e83cbef2c
saoramx <saora.mx@gmail.com>
2019-12-10 02:14:26 -0600

    so. actualizacion de clases

--- src/test/java/stepsdefinition/lecc/home/ScanActionsStepsDefinition.java
+++ src/test/java/stepsdefinition/lecc/home/ScanActionsStepsDefinition.java
@@ -9,23 +9,23 @@ public class ScanActionsStepsDefinition {
     @Steps
     private LeccHomePage leccHomePage;
 
-    @And("^I scan Actions and buttons green icon$")
-    public void scanJsButtonGreenIcon(){
+    @And("^I scan Actions and Buttons green icon$")
+    public void scanActionsAndButtonsGreenIcon(){
         leccHomePage.actionAndButtonScan();
     }
 
-    @And("^I scan Actions and buttons yellow icon$")
-    public void scanJsButtonYellowIcon(){
+    @And("^I scan Actions and Buttons yellow icon$")
+    public void scanActionsAndButtonsYellowIcon(){
         leccHomePage.actionAndButtonScan();
     }
 
-    @And("^I should get yellow icon in the scanning results for Actions and buttons$")
-    public void getYellowJsButtonScanResult(){
+    @And("^I should get yellow icon in the scanning results for Actions and Buttons$")
+    public void getYellowActionsAndButtonScanResult(){
         leccHomePage.scanResultActionsAndButtons("actions and buttons aren’t yet deployed");
     }
 
-    @And("^I should get green icon in the scanning results for Actions and buttons$")
-    public void getGreenJsButtonScanResult(){
+    @And("^I should get green icon in the scanning results for Actions and Buttons$")
+    public void getGreenActionsAndButtonScanResult(){
         leccHomePage.scanResultActionsAndButtons("green..........");
     }
 }
